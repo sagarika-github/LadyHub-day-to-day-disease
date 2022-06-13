feea980540af02a2d56dbe958311740f2c73a063
Sagarika <sagarikamondal197@gmail.com>
2022-06-13 22:37:25 +0530

    Database name changed

--- Database/database.py
+++ Database/database.py
@@ -23,7 +23,7 @@ conn.close()
 
 #Creating table for storing the values in databse.db for K-Nearest Neighbour Algorithm
 import sqlite3 
-conn = sqlite3.connect('database.db') 
+conn = sqlite3.connect('database3.db') 
 c = conn.cursor() 
 c.execute("CREATE TABLE IF NOT EXISTS KNearestNeighbour(Name StringVar,Symtom1 StringVar,Symtom2 StringVar,Symtom3 StringVar,Symtom4 TEXT,Symtom5 TEXT,Disease StringVar)")
 c.execute("INSERT INTO KNearestNeighbour(Name,Symtom1,Symtom2,Symtom3,Symtom4,Symtom5,Disease) VALUES(?,?,?,?,?,?,?)",(NameEn.get(),Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get(),pred4.get()))
